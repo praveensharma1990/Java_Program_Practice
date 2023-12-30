e6e155a1fc4a6e05a7f0b9b3a4cd60dc529935b8
praveensharma1990 <psagra13@gmail.com>
2023-12-31 01:10:54 +0530

    Java Primitive types

---
+++ basic_01/variable.java
@@ -0,0 +1,20 @@
+package basic_01;
+
+public class variable {
+    public static void main(String[] args) {
+        //primitive data types variables
+        int a=10; byte b=3; char cr='A'; double salary= 39999.3;
+        boolean M= true; short sh= 333; float ft= 333f; long lt=44444444L;
+
+        System.out.println("value of int type variable  "+a);
+        System.out.println("value of byte type variable  "+b);
+        System.out.println("value of char type variable  "+cr);
+        System.out.println("value of double type variable  "+salary);
+        System.out.println("value of boolean type variable  "+M);
+        System.out.println("value of short type variable  "+sh);
+        System.out.println("value of float type variable  "+ft);
+        System.out.println("value of long type variable  "+lt);
+    }
+
+
+}
